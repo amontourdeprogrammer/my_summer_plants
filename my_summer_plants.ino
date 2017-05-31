7bca2740afcf06bff3a0ffb063f7b828ad89c533
Lisa Durand <lisa@superdudu.net>
2017-05-31 13:58:10 +0200

    initialize servo position, on pushbutton, servo do one loop

--- my_summer_plants.ino
+++ my_summer_plants.ino
@@ -1,15 +1,19 @@
+
+#include <Servo.h>
+
+Servo myservo; // create servo object to control a servo
+
 // this constant won't change:
 const int  buttonPin = 2;    // the pin that the pushbutton is attached to
-const int ledPin = 13;       // the pin that the LED is attached to
 
 // Variables will change:
 int buttonState = 0;         // current state of the button
+int pos = 0;                // variable to store the servo position
 
 void setup() {
-  // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT);
-  // initialize the LED as an output:
-  pinMode(ledPin, OUTPUT);
+  myservo.attach(3);  // attaches the servo on pin 3 to the servo object
+  pinMode(buttonPin, INPUT);  // initialize the button pin as a input
+  myservo.write(0);
 }
 
 
@@ -18,10 +22,18 @@ void loop() {
   buttonState = digitalRead(buttonPin);
 
   if (buttonState == HIGH) {
-      digitalWrite(ledPin, HIGH);
-      delay(2000);
-      digitalWrite(ledPin, LOW);
-    // Delay a little bit to avoid bouncing
+      
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+    delay(5000);
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+    pos = 0;
   }
-  delay(50);
+  delay(50); // Delay a little bit to avoid bouncing
 }
